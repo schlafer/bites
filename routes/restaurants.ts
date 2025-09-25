104d67a5cdd9924e2bdb5a46d6aa718706c6bdc2
Sazid Mahmud <sazidm@proton.me>
2025-09-25 13:39:26 +0600

    add: redis setup

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.post("/", validate(RestaurantSchema), async (req, res) =>{
   const data = req.body as Restaurant;
+  const client = await initializeRedisClient();
   res.send("Hello, world");
 });
 
